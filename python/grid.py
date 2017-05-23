dc41108cd8da40b8521c26a6351170f7df8c5fc5
jeramy_morrill <jeramy.morrill@analogfolk.com>
2017-05-23 18:25:27 -0400

    adding in distance evaluation math

--- python/grid.py
+++ python/grid.py
@@ -1,4 +1,5 @@
 import math
+from cmath import sqrt
 from colour import Color
 
 class pixel:
@@ -15,46 +16,56 @@ class grid:
     def __init__(self,width,height):
         self.width = width
         self.height = height
-        self.pixels = []
+        # self.pixels = []
         self.neoPixelrings = []
-        for x in range(width):
-            self.pixels.append([])
-            for y in range(height):
-                self.pixels[x].append(pixel(x,y))
+        # for x in range(width):
+        #     self.pixels.append([])
+        #     for y in range(height):
+        #         self.pixels[x].append(pixel(x,y))
 
-    def getPixel(self,x,y):
-        return self.pixels[x][y]
+    # def getPixel(self,x,y):
+    #     return self.pixels[x][y]
 
     def getCenter(self):
         return [self.width / 2, self.height /2]
 
     def insertPixlRing(self,pixelRing,x,y):
         self.neoPixelrings.append(pixelRing)
-        self.getPixel(x,y).setNeoPixelAlias(pixelRing.chainIndex + 0)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 0, x,y)
-        self.getPixel(x-1,y+1).setNeoPixelAlias(pixelRing.chainIndex + 1)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 1, x-1,y+1)
-        self.getPixel(x-2,y+2).setNeoPixelAlias(pixelRing.chainIndex + 2)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 2, x-2,y+2)
-        self.getPixel(x-1,y+3).setNeoPixelAlias(pixelRing.chainIndex + 3)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 3, x-1,y+3)
-        self.getPixel(x,y+4).setNeoPixelAlias(pixelRing.chainIndex + 4)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 4, x,y+4)
-        self.getPixel(x+1,y+3).setNeoPixelAlias(pixelRing.chainIndex + 5)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 5, x+1,y+3)
-        self.getPixel(x+2,y+2).setNeoPixelAlias(pixelRing.chainIndex + 6)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 6, x+2,y+2)
-        self.getPixel(x+1,y+1).setNeoPixelAlias(pixelRing.chainIndex + 7)
-        pixelRing.setPixelActual(pixelRing.chainIndex + 7, x+1,y+1)
+        # self.getPixel(x,y).setNeoPixelAlias(pixelRing.chainOffset + 0)
+        pixelRing.setPixelActual(0, x,y)
+        # self.getPixel(x-1,y+1).setNeoPixelAlias(pixelRing.chainOffset + 1)
+        pixelRing.setPixelActual(1, x-1, y+1)
+        # self.getPixel(x-2,y+2).setNeoPixelAlias(pixelRing.chainOffset + 2)
+        pixelRing.setPixelActual(2, x-2,y+2)
+        # self.getPixel(x-1,y+3).setNeoPixelAlias(pixelRing.chainOffset + 3)
+        pixelRing.setPixelActual(3, x-1,y+3)
+        # self.getPixel(x,y+4).setNeoPixelAlias(pixelRing.chainOffset + 4)
+        pixelRing.setPixelActual(4, x,y+4)
+        # self.getPixel(x+1,y+3).setNeoPixelAlias(pixelRing.chainOffset + 5)
+        pixelRing.setPixelActual(5, x+1,y+3)
+        # self.getPixel(x+2,y+2).setNeoPixelAlias(pixelRing.chainOffset + 6)
+        pixelRing.setPixelActual(6, x+2,y+2)
+        # self.getPixel(x+1,y+1).setNeoPixelAlias(pixelRing.chainOffset + 7)
+        pixelRing.setPixelActual(7, x+1,y+1)
 
 class neoPixelRing:
-    def __init__(self,pixelAmount,chainIndex):
+    def __init__(self,pixelAmount,chainOffset):
         self.pixelAmount = pixelAmount
-        self.chainIndex = chainIndex
-        self.pixelMask = [None] * 8
+        self.chainOffset = chainOffset
+        self.pixelMask = [None] * pixelAmount
+
     def setPixelActual(self,pixel,x,y):
         self.pixelMask[pixel] = [x,y]
 
+    def getActualPixelDistance(self,pixel,x,y):
+        a = self.pixelMask[pixel][0] - x
+        b = self.pixelMask[pixel][1] - y
+        v = (a*a) - (b*b);
+        d = abs(sqrt(v))
+        return d
+
+    def getPixelChainPosition(self,pixel):
+        return pixel + (self.chainOffset * self.pixelAmount)
 
 def create_led_grid(width,height):
     return grid(width,height)
--- python/test.py
+++ python/test.py
@@ -2,8 +2,17 @@ import gradient
 import grid
 
 if __name__ == '__main__':
-    print (gradient.linear_gradient("#0f0f0f","#000000"))
+    # print (gradient.linear_gradient("#0f0f0f","#000000"))
     g = grid.create_led_grid(14,14)
+
     np = grid.neoPixelRing(8,0)
+    np2 = grid.neoPixelRing(8,1)
+    np3 = grid.neoPixelRing(8,2)
+
     g.insertPixlRing(np,7,2)
-    print(np.pixelMask)
+    g.insertPixlRing(np2,2,8)
+    g.insertPixlRing(np3,11,8)
+    center = g.getCenter()
+
+    print(np.getActualPixelDistance(0,center[0],center[1]))
+    # print(np3.pixelMask)
