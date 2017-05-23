fd6dcd54449be0eb5a960284f0552b23af22df91
jeramy_morrill <jeramy.morrill@analogfolk.com>
2017-05-23 16:02:48 -0400

    simple grid functions

---
+++ python/grid.py
@@ -0,0 +1,28 @@
+import math
+from colour import Color
+
+class pixel:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def setPixelColor(self,color):
+        self.color = color
+    def setNeoPixelAlias(self,alias):
+        self.pixelAlias = alias
+
+
+class grid:
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+        self.pixels = []
+        for x in range(width):
+            self.pixels.append([])
+            for y in range(height):
+                self.pixels[x].append(pixel(x,y))
+    def getPixel(self,x,y):
+        return self.pixels[x][y];
+
+
+def create_led_grid(width,height):
+    return grid(width,height)
---
+++ python/test.py
@@ -0,0 +1,7 @@
+import gradient
+import grid
+
+if __name__ == '__main__':
+    print (gradient.linear_gradient("#0f0f0f","#000000"))
+    g = grid.create_led_grid(13,13)
+    print(g)
