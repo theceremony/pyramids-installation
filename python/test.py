1cad26d71debc157670f9cc5992c0a0d827b0ac2
Jeramy Morrill <jeramy@analogfolk.com>
2017-05-25 17:39:18 -0400

    radial gradient drawing

--- python/test.py
+++ python/test.py
@@ -4,38 +4,61 @@ import pygame
 import sys
 
 from cmath import sqrt
+import math
+
+grid_width = 15
+grid_height = 15
+grid_drawing_mulitplier = 10
+
+g = grid.create_led_grid(grid_width,grid_height)
+
+np = grid.neoPixelRing(8,0)
+np2 = grid.neoPixelRing(8,1)
+np3 = grid.neoPixelRing(8,2)
+
+g.insertPixelRing(np,7,2)
+g.insertPixelRing(np2,2,8)
+g.insertPixelRing(np3,11,8)
+center = g.getCenter()
+
+outGradient = gradient.linear_gradient("#11aaff","#222222",10)
+outGradient2 = gradient.polylinear_gradient(["#000000","#000000","#11aaff","#000000"],10)
+
+pygame.init()
+background_colour = (255,255,255)
+(width, height) = (grid_width * grid_drawing_mulitplier, grid_height * grid_drawing_mulitplier)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Tutorial 1')
+screen.fill(background_colour)
+pygame.display.flip()
+
+
+def plotCircle(x0,y0,radius):
+    x = radius
+    y = 0
+    err = 0
+    width = 1
+    height = 1
+    while x >= y:
+
+        pygame.draw.rect(screen,(111,0,0),(x0 + x , y0 + y, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 + y, y0 + x, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 - y, y0 + x, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 - x, y0 + y, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 - x, y0 - y, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 - y, y0 - x, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 + y, y0 - x, width, height))
+        pygame.draw.rect(screen,(111,0,0),(x0 + x, y0 - y, width, height))
+        pygame.display.flip()
+        y +=1
+        if err <= 0:
+            err += 2 * y + 1
+        if err > 0:
+            x -= 1
+            err -= 2 * x + 1
+
 
 if __name__ == '__main__':
-    # print (gradient.linear_gradient("#0f0f0f","#000000"))
-
-    grid_width = 14
-    grid_height = 14
-    grid_drawing_mulitplier = 31
-
-    g = grid.create_led_grid(grid_width,grid_height)
-
-    np = grid.neoPixelRing(8,0)
-    np2 = grid.neoPixelRing(8,1)
-    np3 = grid.neoPixelRing(8,2)
-
-    g.insertPixelRing(np,7,2)
-    g.insertPixelRing(np2,2,8)
-    g.insertPixelRing(np3,11,8)
-    center = g.getCenter()
-
-    gradient = gradient.linear_gradient("#003300","#11aaff")
-    # gradient2 = gradient.polylinear_gradient(["#003300","#11aaff","#003300"],8)
-
-    # print(np3.pixelMask   )
-
-    print(gradient)
-    pygame.init()
-    background_colour = (255,255,255)
-    (width, height) = (grid_width * grid_drawing_mulitplier, grid_height * grid_drawing_mulitplier)
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Tutorial 1')
-    screen.fill(background_colour)
-    pygame.display.flip()
 
     running = True
     while running:
@@ -43,14 +66,15 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill(background_colour)
-        # pygame.draw.rect(screen, (60, 60, 100), (20, 20, 40, 40))
+        # screen.fill(background_colour)
 
         for x in range(grid_width):
             for y in range(grid_height):
-                a = x - (grid_width/2)
-                b = y - (grid_height/2)
-                v = (a*a) - (b*b);
-                d = int(abs(sqrt(v)))
-                pygame.draw.rect(screen, (gradient['r'][d], gradient['g'][d], gradient['b'][d]), (x * grid_drawing_mulitplier, y * grid_drawing_mulitplier, grid_drawing_mulitplier, grid_drawing_mulitplier))
+
+                d1 = math.sqrt((x - grid_width/2) ** 2 + (y - grid_height/2) ** 2)
+                d1 = int(math.floor(float(d1) / (math.sqrt(2) * grid_width/2) * 10))
+                # print(d1)
+                if d1 > len(outGradient2['r'])-1:
+                    d1 = len(outGradient2['r'])-1
+                pygame.draw.rect(screen, (outGradient2['r'][d1], outGradient2['g'][d1], outGradient2['b'][d1]), (x * grid_drawing_mulitplier, y * grid_drawing_mulitplier, grid_drawing_mulitplier, grid_drawing_mulitplier))
         pygame.display.flip()
